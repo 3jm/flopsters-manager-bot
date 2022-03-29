37529105e566014b2c14f5f1b4c6af893bd7a1b2
Unknown <68416923+3jm@users.noreply.github.com>
2022-03-29 04:58:04 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -5,29 +5,43 @@ import asyncio
 import json
 import psutil
 import datetime
+import random
+import datetime
+from datetime import datetime
 import os
 
-# setuop our bot
 bot = commands.Bot(command_prefix=',')
 
-
-# create ready event
 @bot.event
 async def on_ready():
-    # create a list of statuses to cycle through
     statuses = [
         f'Watching {len(bot.users)} users',
         'Made with Love',
         'In Floppa We Trust',
     ]
-    # cycle through statuses
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
-# remove default help command
 bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(
+        description = f'commands with <> are required\ncommands with [] are optional',
+        colour=discord.Colour.blue()
+    )
+    embed.set_author(name=f'Floppsters Help Menu', icon_url=f'{ctx.author.avatar_url}')
+    embed.set_footer(text=f'{ctx.author.name} ({ctx.author.id})', icon_url=f'{ctx.author.avatar_url}')
+    embed.timestamp = datetime.utcnow()
+    embed.set_thumbnail(url=f'{bot.user.avatar_url}')
+    embed.set_image(url='https://splashcord.com/wp-content/uploads/buddypress/groups/36/cover-image/623ab59542212-bp-cover-image.png')
+    embed.add_field(name='Community Commands', value='`art`,`help`,`ping`,`media`,`,floppa`,`,sogga`', inline=False)
+    embed.add_field(name='Moderation Commands', value='`kick <user>`,`ban <user> <reason>`\n`unban <user>`,`lock [channel]`\n`unlock [channel]`,`softban <user> <reason>`', inline=False)
+    embed.add_field(name='Contribution', value='My source code is available on [GitHub](https://github.com/3jm/flopsters-manager-bot)', inline=False)
+    await ctx.send(embed=embed)
+
 
 @bot.command()
 async def art(ctx):
@@ -42,40 +56,34 @@ async def art(ctx):
     except Exception as e:
         await ctx.send(f'{e}')
 
-# create a command if the user runs it, they get a media ping role, and if they already have the role but run it again, remove the role
+Verified_Guilds= [954974341504831539]
+
 @bot.command()
 async def media(ctx):
+    if ctx.guild.id not in Verified_Guilds:
+        await ctx.send('This command is only available in Floppsters! You can join here\nhttps://discord.gg/cVJZhZQjhF')
+        return
     try:
-        # get the role
         role = discord.utils.get(ctx.guild.roles, name='Media')
-        # get the member
         member = ctx.message.author
-        # check if the member has the role
         if role in member.roles:
-            # remove the role
             await member.remove_roles(role)
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}, Removed the Media role.', color = 0xd30059)
             await ctx.send(embed = em)
         else:
-            # add the role
             await member.add_roles(role)
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}, Added the Media role.', color = 0xd30059)
             await ctx.send(embed = em)
     except Exception as e:
         await ctx.send(f'{e}')
 
-# create a command to lock a channel
 @bot.command()
 async def lock(ctx, *, channel : discord.TextChannel=None):
     channel = channel or ctx.channel
     ovrite = channel.overwrites_for(ctx.guild.default_role)
     ovrite.send_messages = False
     if channel == None:
-        # check if the channel is already locked
         if channel.overwrites_for(ctx.guild.default_role).send_messages == False:
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}**, The channel is already locked.**', color = 0xd30059)
             await ctx.send(embed = em)
         else:
@@ -83,9 +91,7 @@ async def lock(ctx, *, channel : discord.TextChannel=None):
             em = discord.Embed(description=f':lock: **<#{channel.id}> has been locked.**', color = 0xd30059)
             await ctx.send(embed = em)
     else:
-        # check if the channel is already locked
         if channel.overwrites_for(ctx.guild.default_role).send_messages == False:
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}**, The channel is already locked.**', color = 0xd30059)
             await ctx.send(embed = em)
         else:
@@ -99,9 +105,7 @@ async def unlock(ctx, *, channel : discord.TextChannel=None):
     ovrite = channel.overwrites_for(ctx.guild.default_role)
     ovrite.send_messages = True
     if channel == None:
-        # check if the channel is already unlocked
         if channel.overwrites_for(ctx.guild.default_role).send_messages == True:
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}**, The channel is already unlocked.**', color = 0xd30059)
             await ctx.send(embed = em)
         else:
@@ -109,15 +113,68 @@ async def unlock(ctx, *, channel : discord.TextChannel=None):
             em = discord.Embed(description=f':unlock: **<#{channel.id}> has been unlocked.**', color = 0xd30059)
             await ctx.send(embed = em)
     else:
-        # check if the channel is already unlocked
         if channel.overwrites_for(ctx.guild.default_role).send_messages == True:
-            # send a message
             em = discord.Embed(description = f'{ctx.author.mention}**, The channel is already unlocked.**', color = 0xd30059)
             await ctx.send(embed = em)
         else:
             await channel.set_permissions(ctx.guild.default_role, overwrite = ovrite)
             em = discord.Embed(description=f':unlock: **<#{channel.id}> has been unlocked.**', color = 0xd30059)
             await ctx.send(embed = em)
+
+@bot.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    try:
+        if ctx.author.guild_permissions.ban_members == True:
+            await member.ban(reason=reason)
+            await ctx.send(':thumbsup:')
+        else:
+            await ctx.send(':thumbsdown:')
+    except Exception as e:
+        print(f'Error: {e}')
+
+@bot.command()
+async def unban(ctx, *, member):
+    try:
+        if ctx.author.guild_permissions.ban_members == True:
+            banned_users = await ctx.guild.bans()
+            for ban_entry in banned_users:
+                user = ban_entry.user
+                if (user.name, user.discriminator) == member.split('#'):
+                    await ctx.guild.unban(user)
+                    await ctx.send(':thumbsup:')
+                    return
+        await ctx.send(':thumbsdown:')
+    except Exception as e:
+        print(f'Error: {e}')
+
+@bot.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    try:
+        if ctx.author.guild_permissions.kick_members == True:
+            await member.kick(reason=reason)
+            await ctx.send(':thumbsup:')
+        else:
+            await ctx.send(':thumbsdown:')
+    except Exception as e:
+        print(f'Error: {e}')
+
+@bot.command()
+async def softban(ctx, member : discord.Member, *, reason=None):
+    try:
+        if ctx.author.guild_permissions.ban_members == True:
+            await member.ban(reason=reason)
+            await ctx.guild.unban(member)
+            await ctx.send(':thumbsup:')
+        else:
+            await ctx.send(':thumbsdown:')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+@bot.command()
+async def ping(ctx):
+    latency = bot.latency
+    await ctx.send(f'Discord: {round(bot.latency * 1000)}ms')
 
 @bot.event
 async def on_guild_join(guild):
@@ -135,5 +192,22 @@ async def on_guild_join(guild):
     with open('db.json', 'w') as f:
         json.dump(db, f, indent=4)
 
-# login to bot
+@bot.command()
+async def floppa(ctx):
+    msg = await ctx.send('1 second please...')
+    api = 'https://api.tenor.com/v1/search?q=floppa&key=LIVDSRZULELA&limit=50'
+    data = requests.get(api).json()
+    image = random.choice(data['results'])
+    url = image['media'][0]['gif']['url']
+    await msg.edit(content=url)
+
+@bot.command()
+async def sogga(ctx):
+    msg = await ctx.send('1 second please...')
+    api = 'https://api.tenor.com/v1/search?q=sogga&key=LIVDSRZULELA&limit=50'
+    data = requests.get(api).json()
+    image = random.choice(data['results'])
+    url = image['media'][0]['gif']['url']
+    await msg.edit(content=url)
+
 bot.run('')
